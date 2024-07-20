82a2cfda33800f29320ee4a5282f85cd8e520111
iamsampath <sam@gmail.com>
2024-07-21 00:26:59 +0530

    last update in page

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
 
-const ShoeCard = () => {
+
+const ShoeCard = ({imgURL , changeBigShoeImage , bigShoeImage}) => {
+const handleClick = () => {
+    if(bigShoeImage !== imgURL.bigShoe){
+        changeBigShoeImage(imgURL.bigShoe)
+    }
+}
+
   return (
-    <div>ShoeCard</div>
+    <div className={`border-2 rounded-xl ${bigShoeImage === imgURL ? 'border-coral-red' : 'border-transparent' } cursor-pointer max-sm:flex-1`} 
+    onClick={handleClick}
+    > <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4 ">
+        <img src={imgURL.thumbnail} alt="Shoe Collection" width={127} height={103} className="object-contain"></img>
+        </div></div>
   )
 }
 
