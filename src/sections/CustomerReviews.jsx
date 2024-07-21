b61c34ca4d8b9b20d76ec5474c505bc805e06c77
iamsampath <sam@gmail.com>
2024-07-21 23:02:40 +0530

    Added new last second stage

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,8 +1,23 @@
+import ReviewCard from "../components/ReviewCard"
+import { reviews } from "../constants"
 
 
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+    <section className="max-container">
+        <h3 className="font-palanquin text-center font-bold text-4xl">
+          What Our?
+          <span className="text-coral-red"> Customers </span>
+           Say?
+        </h3>
+        <p className="info-text m-auto max-w-lg text-center">Hear genuine stories of our customers</p>
+        <div className="mt-24 flex flex-1  justify-evenly items-center max-lg:flex-col gap-14">
+          {reviews.map((review) => (<ReviewCard key={review.customerName} imgURL={review.imgURL} customerName={review.customerName} rating={review.rating} feedback={review.feedback}>
+
+          </ReviewCard>))}
+
+        </div>
+      </section>
   )
 }
 
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,9 +1,37 @@
-
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images"
+import Button from '../components/Button'
 
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+   <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+    <div className="flex-1">
+      <img src={offer} width={773} height={687} className="object-contain w-full"></img>
+    </div>
+    <div className="flex flex-1 flex-col">
+     
+     <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg"> 
+       We Provide You
+       <br></br>
+       <span className="text-coral-red ">Special</span>
+       <span className="text-coral-red ">Offer</span>
+       Shoes
+     </h2>
+     <p className='mt-4 lg:max-w-lg info-text'>
+           Ensuring premium comfort and style, our meticulously crafted footwear
+           is designed to elevate your experience, providing you with unmatched
+           quality, innovation, and a touch of elegance.
+         </p>
+         <p className='mt-6 lg:max-w-lg info-text'>
+           Our dedication to detail and excellence ensures your satisfaction
+         </p>
+         <div className='mt-11 flex flex-wrap gap-4'>
+           <Button label='Shop Now' iconURL={arrowRight} />
+           <Button label="Learn more" backgroundColor="bg-white" borderColor="border-slate-gray" textColor ="text-slate-gray"></Button>
+           </div>
+       </div>
+   </section>
   )
 }
 
-export default SpecialOffer
+export default SpecialOffer;
